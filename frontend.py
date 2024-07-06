8f459fb80bbc83583c854d37cdf47235be92acbf
nodbew <165175929+nodbew@users.noreply.github.com>
2024-07-06 20:16:19 +0900

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -58,5 +58,4 @@ def rerun():
     st.session_state.board, st.session_state.start_pos, st.session_state.flag_positions = core.generate_map(**st.session_state.args)
     st.session_state.commands = list()
     st.session_state.executer = None
-    st.rerun()
     return
